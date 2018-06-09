a1c2d587aaa9eb4140c9139612611d50cd753466
nik <nikitasavelev11@gmail.com>
2018-06-09 18:10:31 +0300

    d
    d

--- app/models/db.js
+++ app/models/db.js
@@ -2,9 +2,9 @@ var pg = require('pg');
 
 var config = require('../../config/database');
 
-/*if (process.env.DATABASE_URL) {
+if (process.env.DATABASE_URL) {
     config = parse(process.env.DATABASE_URL);
-}*/
+}
 
 var pool = new pg.Pool(config);
 
