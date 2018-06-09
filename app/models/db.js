b6f5c0fb1e7389bf26c4ac0c32f9d1f4f96e20f5
nik <nikitasavelev11@gmail.com>
2018-06-09 17:58:08 +0300

    gfg
    gfg

--- app/models/db.js
+++ app/models/db.js
@@ -2,10 +2,11 @@ var pg = require('pg');
 
 var config = require('../../config/database');
 
-if (process.env.DATABASE_URL) {
+/*if (process.env.DATABASE_URL) {
     config = parse(process.env.DATABASE_URL);
-}
+}*/
 
 var pool = new pg.Pool(config);
 
 module.exports = pool;
+
